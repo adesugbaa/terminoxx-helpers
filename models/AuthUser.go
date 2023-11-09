fd194253d11f301cdf471137a48996877fd5ec6b
Ayo Adesugba <ayoadesugba@gmail.com>
2023-11-09 18:39:19 -0500

    modify auth user

--- models/AuthUser.go
+++ models/AuthUser.go
@@ -5,3 +5,11 @@ type AuthUser struct {
 	Email    string
 	Role     string
 }
+
+func NewAuthUser(username, email, role string) *AuthUser {
+	return &AuthUser{
+		Username: username,
+		Email:    email,
+		Role:     role,
+	}
+}
